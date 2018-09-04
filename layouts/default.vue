e2d0c05ede5d12eda489e58d9086fd74f2f7129e
BoomGIJO <39892088+BoomGIJO@users.noreply.github.com>
2018-09-04 15:44:40 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,79 @@
 <template>
+
   <v-app>
-    <v-navigation-drawer v-model="drawer" app right/>
-    <v-toolbar app>
-      <v-toolbar-title>ระบบจัดการพนักงานบริษัท</v-toolbar-title>
+
+    <v-navigation-drawer v-model="drawer" app right>
+      <v-list>
+          <v-list-tile v-for="item in items" :key="item.title">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title><nuxt-link :to="item.url">{{ item.title }}</nuxt-link></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+        </v-navigation-drawer>
+    <v-toolbar app class="grey darken-4">
+      <v-toolbar-title></v-toolbar-title>
       <v-spacer/>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
     </v-toolbar>
     <v-content>
       <nuxt/>
     </v-content>
+   <v-footer
+    dark
+    height="auto"
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="teal">
+        <strong class="subheading"> </strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="grey darken-4"
+          dark
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-actions class="grey darken-4 justify-center">
+        &copy;<strong></strong>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+  
+  
+    
   </v-app>
+   
 </template>
 
 <script>
 export default {
+   
+  data() {
+    return {
+      
+      items: [
+         { title: 'Home', icon: 'home', url: '/' },
+         { title: 'Search', icon: 'group', url: '/search' },
+         { title: 'Login', icon: 'security', url: '/login' },
+       ],
+      
+     }
+     
+  },
   computed: {
     online: {
       get() {
@@ -32,18 +92,15 @@ export default {
       },
     },
   },
-
   mounted() {
     this.$store.commit('setOnline', window.navigator.onLine)
     window.addEventListener('offline', this.toggleNetworkStatus)
     window.addEventListener('online', this.toggleNetworkStatus)
   },
-
   beforeDestroyed() {
     window.removeEventListener('offline', this.toggleNetworkStatus)
     window.removeEventListener('online', this.toggleNetworkStatus)
   },
-
   methods: {
     toggleNetworkStatus({ type }) {
       this.online = type === 'online'
